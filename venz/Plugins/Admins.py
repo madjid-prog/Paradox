464dff538626fdd87f8d0e05f50c1e45d6bb3db3
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-01-19 22:46:32 +0800

    Update Admins.py

--- venz/Plugins/Admins.py
+++ venz/Plugins/Admins.py
@@ -9,26 +9,26 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, KeyboardButton, Message,
                             ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
-from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, db_mem
-from Yukki.Core.PyTgCalls import Queues
-from Yukki.Core.PyTgCalls.Converter import convert
-from Yukki.Core.PyTgCalls.Downloader import download
-from Yukki.Core.PyTgCalls.Yukki import (pause_stream, resume_stream, skip_video_stream,
+from Venz import BOT_USERNAME, MUSIC_BOT_NAME, app, db_mem
+from Venz.Core.PyTgCalls import Queues
+from Venz.Core.PyTgCalls.Converter import convert
+from Venz.Core.PyTgCalls.Downloader import download
+from Venz.Core.PyTgCalls.Yukki import (pause_stream, resume_stream, skip_video_stream,
                                         skip_stream, stop_stream)
-from Yukki.Database import (is_active_chat, is_music_playing, music_off, remove_active_video_chat,
+from Venz.Database import (is_active_chat, is_music_playing, music_off, remove_active_video_chat,
                             music_on, remove_active_chat)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Inline import audio_markup, primary_markup, secondary_markup2
-from Yukki.Utilities.changers import time_to_seconds
-from Yukki.Utilities.chat import specialfont_to_normal
-from Yukki.Utilities.theme import check_theme
-from Yukki.Utilities.thumbnails import gen_thumb
-from Yukki.Utilities.timer import start_timer
-from Yukki.Utilities.youtube import get_yt_info_id
+from Venz.Decorators.admins import AdminRightsCheck
+from Venz.Decorators.checker import checker, checkerCB
+from Venz.Inline import audio_markup, primary_markup, secondary_markup2
+from Venz.Utilities.changers import time_to_seconds
+from Venz.Utilities.chat import specialfont_to_normal
+from Venz.Utilities.theme import check_theme
+from Venz.Utilities.thumbnails import gen_thumb
+from Venz.Utilities.timer import start_timer
+from Venz.Utilities.youtube import get_yt_info_id
 
 
-from Yukki.Utilities.youtube import get_m3u8
+from Venz.Utilities.youtube import get_m3u8
 
 loop = asyncio.get_event_loop()
 
