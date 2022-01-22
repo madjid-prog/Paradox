db993ab5c3212f44dd44171c8fcdab4e31ecc0bf
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-01-22 19:01:11 +0800

    Update Developer.py

--- venz/Plugins/Developer.py
+++ venz/Plugins/Developer.py
@@ -13,8 +13,8 @@ from pyrogram.errors import MessageNotModified
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             Message, ReplyKeyboardMarkup)
 
-from Yukki import SUDOERS, app
-from Yukki.Utilities.tasks import add_task, rm_task
+from Venz import SUDOERS, app
+from Venz.Utilities.tasks import add_task, rm_task
 
 # Eval and Sh module from WBB
 
