00cb0cd0a1c88152f8703ce4e2ecb9323f2667e3
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-01-22 21:04:37 +0800

    Update Stream.py

--- venz/Plugins/Stream.py
+++ venz/Plugins/Stream.py
@@ -4,23 +4,23 @@ from pyrogram import filters
 from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto, Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove,
                             Voice)
 from youtube_search import YoutubeSearch
-from Yukki.Database import is_on_off, get_active_video_chats, get_video_limit, is_active_video_chat
-from Yukki import (BOT_USERNAME, DURATION_LIMIT, DURATION_LIMIT_MIN,
+from Venz.Database import is_on_off, get_active_video_chats, get_video_limit, is_active_video_chat
+from Venz import (BOT_USERNAME, DURATION_LIMIT, DURATION_LIMIT_MIN,
                    MUSIC_BOT_NAME, app, db_mem)
-from Yukki.Core.PyTgCalls.Converter import convert
-from Yukki.Core.PyTgCalls.Downloader import download
-from Yukki.Decorators.assistant import AssistantAdd
-from Yukki.Decorators.checker import checker
-from Yukki.Decorators.permission import PermissionCheck
-from Yukki.Inline import (playlist_markup, search_markup, search_markup2, stream_quality_markup, choose_markup, livestream_markup,
+from Venz.Core.PyTgCalls.Converter import convert
+from Venz.Core.PyTgCalls.Downloader import download
+from Venz.Decorators.assistant import AssistantAdd
+from Venz.Decorators.checker import checker
+from Venz.Decorators.permission import PermissionCheck
+from Venz.Inline import (playlist_markup, search_markup, search_markup2, stream_quality_markup, choose_markup, livestream_markup,
                           url_markup, url_markup2)
-from Yukki.Utilities.changers import seconds_to_min, time_to_seconds
-from Yukki.Utilities.chat import specialfont_to_normal
-from Yukki.Utilities.videostream import start_video_stream, start_live_stream
-from Yukki.Utilities.theme import check_theme
-from Yukki.Utilities.thumbnails import gen_thumb
-from Yukki.Utilities.url import get_url
-from Yukki.Utilities.youtube import (get_yt_info_id, get_yt_info_query, get_m3u8, 
+from Venz.Utilities.changers import seconds_to_min, time_to_seconds
+from Venz.Utilities.chat import specialfont_to_normal
+from Venz.Utilities.videostream import start_video_stream, start_live_stream
+from Venz.Utilities.theme import check_theme
+from Venz.Utilities.thumbnails import gen_thumb
+from Venz.Utilities.url import get_url
+from venz.Utilities.youtube import (get_yt_info_id, get_yt_info_query, get_m3u8, 
                                      get_yt_info_query_slider)
 
 from youtubesearchpython import VideosSearch
@@ -30,20 +30,20 @@ loop = asyncio.get_event_loop()
 __MODULE__ = "VideoCalls"
 __HELP__ = f"""
 
-/play [Reply to any Video] or [YT Link] or [Music Name]
-- Stream Video on Voice Chat
+/play [Balas ke Video apa pun] atau [YT link] atau [Music Name]
+- Streaming Video di Obrolan Suara
 
-**For Sudo User:-**
+**Untuk Pengguna Sudo:-**
 
 /set_video_limit [Number of Chats]
-- Set a maximum Number of Chats allowed for Video Calls at a time.
+- Tetapkan Jumlah Obrolan maksimum yang diizinkan untuk Panggilan Video dalam satu waktu.
 
 
 """
 
 
 
-@app.on_callback_query(filters.regex(pattern=r"Yukki"))
+@app.on_callback_query(filters.regex(pattern=r"Venz"))
 async def choose_playmode(_, CallbackQuery):
     await CallbackQuery.answer()
     callback_data = CallbackQuery.data.strip()
@@ -51,7 +51,7 @@ async def choose_playmode(_, CallbackQuery):
     videoid, duration, user_id = callback_request.split("|")
     if CallbackQuery.from_user.id != int(user_id):
         return await CallbackQuery.answer(
-            "This is not for you! Search You Own Song.", show_alert=True
+            "Ini bukan untukmu! Cari Lagu Anda Sendiri.", show_alert=True
         )
     buttons = choose_markup(videoid, duration, user_id)
     await CallbackQuery.edit_message_reply_markup(
@@ -65,20 +65,20 @@ async def quality_markup(_, CallbackQuery):
     print(limit)
     if not limit:
         await CallbackQuery.message.delete()
-        return await CallbackQuery.message.reply_text("**No Limit Defined for Video Calls**\n\nSet a Limit for Number of Maximum Video Calls allowed on Bot by /set_video_limit [Sudo Users Only]")
+        return await CallbackQuery.message.reply_text("**Tidak Ada Batas yang Ditetapkan untuk Panggilan Video**\n\nTetapkan Batas Jumlah Panggilan Video Maksimum yang diizinkan di Bot dengan /set_video_limit [Khusus Pengguna Sudo]")
     count = len(await get_active_video_chats())
     print(count)
     if int(count) == int(limit):
         if await is_active_video_chat(CallbackQuery.message.chat.id):
             pass
         else:
-            return await CallbackQuery.answer("Sorry! Bot only allows limited number of video calls due to CPU overload issues. Other chats are using video call right now. Try switching to audio or try again later", show_alert=True)
+            return await CallbackQuery.answer("Maaf! Bot hanya mengizinkan panggilan video dalam jumlah terbatas karena masalah kelebihan CPU. Obrolan lain menggunakan panggilan video sekarang. Coba beralih ke audio atau coba lagi nanti", show_alert=True)
     if CallbackQuery.message.chat.id not in db_mem:
         db_mem[CallbackQuery.message.chat.id] = {}
     try:
         read1 = db_mem[CallbackQuery.message.chat.id]["live_check"]
         if read1:
-            return await CallbackQuery.answer("Live Streaming Playing...Stop it to play music", show_alert=True)
+            return await CallbackQuery.answer("Pemutaran Live Streaming... Hentikan untuk memutar musik", show_alert=True)
         else:
             pass
     except:
@@ -89,7 +89,7 @@ async def quality_markup(_, CallbackQuery):
     videoid, duration, user_id = callback_request.split("|")
     if CallbackQuery.from_user.id != int(user_id):
         return await CallbackQuery.answer(
-            "This is not for you! Search You Own Song.", show_alert=True
+            "Ini bukan untukmu! Cari Lagu Anda Sendiri.", show_alert=True
         )
     buttons = stream_quality_markup(videoid, duration, user_id)
     await CallbackQuery.edit_message_reply_markup(
@@ -120,7 +120,7 @@ async def Live_Videos_Stream(_, CallbackQuery):
     quality, videoid, duration, user_id = callback_request.split("|")
     if CallbackQuery.from_user.id != int(user_id):
         return await CallbackQuery.answer(
-            "This is not for you! Search You Own Song.", show_alert=True
+            "Ini bukan untukmu! Cari Lagu Anda Sendiri.", show_alert=True
         )
     await CallbackQuery.message.delete()
     title, duration_min, duration_sec, thumbnail = get_yt_info_id(videoid)
@@ -130,7 +130,7 @@ async def Live_Videos_Stream(_, CallbackQuery):
     thumb = await gen_thumb(thumbnail, title, user_id, theme, chat_title)
     nrs, ytlink = await get_m3u8(videoid)
     if nrs == 0:
-        return await CallbackQuery.message.reply_text("Video Formats not Found..")
+        return await CallbackQuery.message.reply_text("Format Video tidak Ditemukan..")
     await start_live_stream(CallbackQuery, quality, ytlink, thumb, title, duration_min, duration_sec, videoid)
 
 
@@ -146,7 +146,7 @@ async def Videos_Stream(_, CallbackQuery):
     quality, videoid, duration, user_id = callback_request.split("|")
     if CallbackQuery.from_user.id != int(user_id):
         return await CallbackQuery.answer(
-            "This is not for you! Search You Own Song.", show_alert=True
+            "Ini bukan untukmu! Cari Lagu Anda Sendiri.", show_alert=True
         )
     if str(duration) == "None":
         buttons = livestream_markup(quality, videoid, duration, user_id)
@@ -155,7 +155,7 @@ async def Videos_Stream(_, CallbackQuery):
     title, duration_min, duration_sec, thumbnail = get_yt_info_id(videoid)
     if duration_sec > DURATION_LIMIT:
         return await CallbackQuery.message.reply_text(
-            f"**Duration Limit Exceeded**\n\n**Allowed Duration: **{DURATION_LIMIT_MIN} minute(s)\n**Received Duration:** {duration_min} minute(s)"
+            f"**Batas Durasi Terlampaui**\n\n**Durasi yang Diizinkan: **{DURATION_LIMIT_MIN} minute(s)\n**Durasi yang Diterima:** {duration_min} minute(s)"
         )
     await CallbackQuery.answer(f"Processing:- {title[:20]}", show_alert=True)
     theme = await check_theme(chat_id)
@@ -163,5 +163,5 @@ async def Videos_Stream(_, CallbackQuery):
     thumb = await gen_thumb(thumbnail, title, user_id, theme, chat_title)
     nrs, ytlink = await get_m3u8(videoid)
     if nrs == 0:
-        return await CallbackQuery.message.reply_text("Video Formats not Found..")
+        return await CallbackQuery.message.reply_text("Format Video tidak Ditemukan..")
     await start_video_stream(CallbackQuery, quality, ytlink, thumb, title, duration_min, duration_sec, videoid)
