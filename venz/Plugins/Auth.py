1cbcf482b354b68eaacb885dd3214ee29d6fd817
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-01-22 18:55:56 +0800

    Update Auth.py

--- venz/Plugins/Auth.py
+++ venz/Plugins/Auth.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Yukki import SUDOERS, app
-from Yukki.Database import (_get_authusers, delete_authuser, get_authuser,
+from Venz import SUDOERS, app
+from Venz.Database import (_get_authusers, delete_authuser, get_authuser,
                             get_authuser_count, get_authuser_names,
                             save_authuser)
-from Yukki.Decorators.admins import AdminActual
-from Yukki.Utilities.changers import (alpha_to_int, int_to_alpha,
+from Venz.Decorators.admins import AdminActual
+from Venz.Utilities.changers import (alpha_to_int, int_to_alpha,
                                       time_to_seconds)
 
 __MODULE__ = "Auth Users"
